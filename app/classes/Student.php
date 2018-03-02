8ae1f267368c6df9dfbd6d714969da7b2b10ea1a
Mamunur Rashid Mamun <rmamunur6@gmail.com>
2018-03-02 12:22:58 +0600

    php and sql

---
+++ app/classes/Student.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Student
+{
+    public function saveStudentInfo($data)
+    {
+        $link=mysqli_connect('localhost','root','','student');
+        $sql="INSERT INTO students(name,email,mobile) VALUES ('$data[name]','$data[email]','$data[mobile]')";
+
+        if(mysqli_query($link,$sql))
+        {
+            $message="Student info saved successfully";
+            return $message;
+        }
+        else
+        {
+            die("Query Problem".mysqli_error($link));
+        }
+
+    }
+
+    public function getAllStudentInfo()
+    {
+        $link=mysqli_connect('localhost','root','','student');
+        $sql="SELECT * FROM students";
+
+        if(mysqli_query($link,$sql))
+        {
+            $queryResult=mysqli_query($link,$sql);
+            return $queryResult;
+        }
+        else
+        {
+            die("Query Problem".mysqli_error($link));
+        }
+
+    }
+
+}
